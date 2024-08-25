8590ed038e24f1980705d299f3bfff8008c46106
Lokesh Burade <lokeshburade494@gmail.com>
2024-08-25 10:06:11 +0530

    Initial deployment

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         formData.append('image', image);
 
         try {
-            const response = await axios.post('http://localhost:5000/upload', formData, {
+            const response = await axios.post('https://ocr-to-text-github-io.onrender.com/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
